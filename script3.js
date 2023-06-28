415932e0b05faee440e84f2856a7d638a80e32f5
ryanng1999 <88872675+ryanng1999@users.noreply.github.com>
2023-06-28 18:26:26 -0400

    Add files via upload

--- script3.js
+++ script3.js
@@ -8,7 +8,7 @@ var images = [
   "./images/Relaxed.jpeg",
   "./images/RandomObjects.jpg",
   "./images/KitchenObjects.jpg",
-  "./images/DuanePark.JPEG",
+  "./images/DuanePark.png",
   "./images/DivisionStreet.JPEG"];
 
 var num = 0;  // num holds the current image
